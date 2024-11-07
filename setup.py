89a62c18078072c001e3034ccbd6716e9f9bf21c
Gasper Podobnik <gasper.podobnik@gmail.com>
2024-11-07 16:17:34 +0100

    add setup.py for package installation

---
+++ setup.py
@@ -0,0 +1,14 @@
+"""PyPI package definition."""
+
+from setuptools import setup
+
+setup(name="Distance-based metrics for segmentation evaluation",
+      version="0.1",
+      description=(
+          "Library to compute distance-based performance metrics for image segmentation tasks."),
+      url="https://github.com/gasperpodobnik/MeshMetrics",
+      author="Gasper Podobnik",
+      license="Apache License, Version 2.0",
+      packages=["MeshMetrics"],
+      install_requires=["numpy", "SimpleITK", "SimpleITKUtilities", "vtk", ]
+      )
