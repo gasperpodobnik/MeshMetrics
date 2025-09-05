90390e2fd72c92caf59b64a2e46723c899e68f1e
Gasper Podobnik <gasper.podobnik@gmail.com>
2025-09-05 14:12:57 +0200

    version update

--- setup.py
+++ setup.py
@@ -2,13 +2,20 @@
 
 from setuptools import setup
 
-setup(name="MeshMetrics",
-      version="0.1",
-      description=(
-          "Library to compute distance-based performance metrics for image segmentation tasks."),
-      url="https://github.com/gasperpodobnik/MeshMetrics",
-      author="Gasper Podobnik",
-      license="Apache License, Version 2.0",
-      packages=["MeshMetrics"],
-      install_requires=["numpy", "SimpleITK", "SimpleITKUtilities", "vtk", ]
-      )
+setup(
+    name="MeshMetrics",
+    version="0.2.0",
+    description=(
+        "Library to compute distance-based performance metrics for image segmentation tasks."
+    ),
+    url="https://github.com/gasperpodobnik/MeshMetrics",
+    author="Gasper Podobnik",
+    license="Apache License, Version 2.0",
+    packages=["MeshMetrics"],
+    install_requires=[
+        "numpy",
+        "SimpleITK",
+        "SimpleITKUtilities",
+        "vtk",
+    ],
+)
